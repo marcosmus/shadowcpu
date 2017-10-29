0909bd538dbcb8f3cfae296db8413d83b25ed2a1
mfaiola <mfaiola1@gmail.com>
2017-10-29 12:59:02 -0400

    Update cpulimiter.sh

--- cpulimiter.sh
+++ cpulimiter.sh
@@ -3,7 +3,7 @@ cpu=`cat cpuname.txt`
 while [ 1=1 ]
 do
 	wait=$(shuf -i2-8 -n1)
-	load=$(shuf -i350-390 -n1)
+	load=$(shuf -i360-395 -n1)
 	cpulimit -e ${cpu} -l ${load} -b -q
 	sleep ${wait}
 	pkill cpulimit
